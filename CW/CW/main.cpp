3589d74036f73bc2ede3604a4e78328ebabf92b8
Yozhylo <polidar2012@gmail.com>
2021-05-24 01:03:35 +0300

    First

--- CW/CW/main.cpp
+++ CW/CW/main.cpp
@@ -1,6 +1,25 @@
+#include <glew.h>
 #include <glfw3.h>
+
+int windowW = 500, windowH = 500;
+GLFWwindow* game;
 
 void main()
 {
-	
+	glfwInit();
+	game = glfwCreateWindow(windowW, windowH, "Labyrinth", NULL, NULL);
+	while (!glfwWindowShouldClose(game))
+	{
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+			glColor3f(1,1,1); glVertex2f(1,1);
+			glVertex2f(0, 1);
+			glVertex2f(1, 0);
+		glEnd();
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(game);
+	glfwTerminate();
 }
